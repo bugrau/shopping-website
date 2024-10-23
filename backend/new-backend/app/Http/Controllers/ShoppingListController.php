f4edaf8f31cbb277f995211eff5c920656dd8e9a
Bugra Unaltay <bugraunaltay@gmail.com>
2024-10-23 21:37:32 +0300

    Create ShoppingListController for CRUD operations

--- backend/new-backend/app/Http/Controllers/ShoppingListController.php
+++ backend/new-backend/app/Http/Controllers/ShoppingListController.php
@@ -1,41 +1,118 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\ShoppingListItem;
-use Illuminate\Http\Request;
-
-class ShoppingListController extends Controller
-{
-    public function index()
-    {
-        return ShoppingListItem::all();
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string',
-            'quantity' => 'required|integer|min:1',
-        ]);
-
-        return ShoppingListItem::create($request->all());
-    }
-
-    public function update(Request $request, ShoppingListItem $item)
-    {
-        $request->validate([
-            'purchased' => 'required|boolean',
-        ]);
-
-        $item->update($request->all());
-        return $item;
-    }
-
-    public function destroy(ShoppingListItem $item)
-    {
-        $item->delete();
-        return response()->json(null, 204);
-    }
-}
-
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+
+
+use App\Models\ShoppingListItem;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Log;
+
+
+
+class ShoppingListController extends Controller
+
+{
+
+    public function index()
+
+    {
+
+        $user = Auth::user();
+
+        Log::info('Fetching items for user: ' . $user->id);
+
+        $items = $user->shoppingListItems;
+
+        Log::info('Items fetched: ' . $items->count());
+
+        Log::info('Items: ' . json_encode($items));
+
+        return response()->json($items);
+
+    }
+
+
+
+    public function store(Request $request)
+
+    {
+
+        $user = Auth::user();
+
+        Log::info('Creating item for user: ' . $user->id);
+
+        $validatedData = $request->validate([
+
+            'name' => 'required|string|max:255',
+
+            'quantity' => 'required|integer|min:1',
+
+            'purchased' => 'boolean',
+
+        ]);
+
+
+
+        $item = $user->shoppingListItems()->create($validatedData);
+
+        Log::info('Item created: ' . $item->id);
+
+        return response()->json($item, 201);
+
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $user = Auth::user();
+        Log::info('Updating item ' . $id . ' for user: ' . $user->id);
+        $item = $user->shoppingListItems()->find($id);
+        
+        if (!$item) {
+            Log::warning('Item not found or not owned by user. Item ID: ' . $id . ', User ID: ' . $user->id);
+            return response()->json(['message' => 'Item not found'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'quantity' => 'integer|min:1',
+            'purchased' => 'boolean',
+        ]);
+
+        $item->update($validatedData);
+        Log::info('Item updated: ' . $item->id);
+        return response()->json($item);
+    }
+
+
+
+    public function destroy($id)
+
+    {
+
+        $user = Auth::user();
+
+        Log::info('Deleting item ' . $id . ' for user: ' . $user->id);
+
+        $item = $user->shoppingListItems()->findOrFail($id);
+
+        $item->delete();
+
+        Log::info('Item deleted: ' . $id);
+
+        return response()->json(null, 204);
+
+    }
+
+}
+
+
+
